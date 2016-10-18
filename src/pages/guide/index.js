a08a25089813c2b2f41eb2e8b7e9aeb1679eba11
liufang <liufang@mapbar.com>
2016-10-18 11:26:40 +0800

    添加登录注册相关

--- src/pages/guide/index.js
+++ src/pages/guide/index.js
@@ -15,6 +15,18 @@ import PhoneChkCodeInput from '../../components/Inputs/PhoneChkCode';
 import TopBanner from '../../components/TopBanner';
 import Alert from '../../components/Modals/Alert';
 
+
+import user from '../user/index'
+import Login from '../user/Login';
+import QuickLogin from '../user/QuickLogin';
+import SaveTrueName from '../user/SaveTrueName';
+import Reg from '../user/Reg';
+import RegCheckCode from '../user/RegCheckCode';
+import ServiceProvision from '../user/ServiceProvision';
+import FindPassword from '../user/FindPassword';
+import FindPasswordCheckCode from '../user/FindPasswordCheckCode';
+import FindPasswordNewPassword from '../user/FindPasswordNewPassword';
+
 export default class Guide extends Component {
     constructor(props) {
         super(props);
@@ -56,6 +68,18 @@ export default class Guide extends Component {
                     <Alert visible={this.state.alertActive} contentType="input" placeholder="自定义内容" onClose={(()=>{this.setState({alertActive:false})}).bind(this)}>是否关闭?</Alert>
                     <TopBanner title="topbanner" leftTitle/>
                 </View>
+                <Text style={{fontSize:18,color:'red'}}>注册登录相关</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(user)}}>0、登录</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(Login)}}>1、手机密码登录</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(QuickLogin)}}>2、手机快捷登录</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(SaveTrueName)}}>3、输入真实姓名（快捷登录未填写姓名）</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(Reg)}}>4、注册</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(RegCheckCode)}}>5、注册（填写验证码）</Text>
+                <Text  {...this.props} onPress={() => {this.toPage(ServiceProvision)}}>6、服务条款</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(FindPassword)}}>7、找回密码</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(FindPasswordCheckCode)}}>8、找回密码-填写短信验证码</Text>
+                <Text  style={styles.over} {...this.props} onPress={() => {this.toPage(FindPasswordNewPassword)}}>9、找回密码-输入新密码</Text>
+                <Text> </Text>
             </ScrollView>
         )
     }
