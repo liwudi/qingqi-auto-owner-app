d7190a4835ed90fc652be9519186737b62db4b5b
liufang <liufang@mapbar.com>
2016-12-18 21:52:52 +0800

    添加车辆

--- src/pages/home/monitor/components/MyCarItem.js
+++ src/pages/home/monitor/components/MyCarItem.js
@@ -10,7 +10,7 @@ import {
 import {queryRealTimeCar} from '../../../../services/MonitorService';
 
 import Item from '../../my-car/components/MyCarItem';
-const TIMEOUT = 30; //间隔30秒刷新
+const TIMEOUT = 5; //间隔30秒刷新
 export default class MyCarItem extends Component {
     constructor() {
         super();
@@ -71,12 +71,14 @@ export default class MyCarItem extends Component {
         let cidx = props.router.currentIndex();
         if(this.ridx === null) this.ridx = cidx;
         console.info('update -----------------------------------')
-        this.timer && clearTimeout(this.timer);
+        //this.timer && clearTimeout(this.timer);
         if(cidx === this.ridx) {
-            this.requestStart();
-            setTimeout(() => {
-                this.fetchData();
-            }, Math.random() * 2000);
+            if(this.stopRequest) {
+                this.requestStart();
+                setTimeout(() => {
+                    this.fetchData();
+                }, Math.random() * 2000);
+            }
         } else {
             this.requestStop();
         }
@@ -84,7 +86,7 @@ export default class MyCarItem extends Component {
     }
     componentWillReceiveProps(props) {
         props.data && this.setData(props.data);
-        this.requestStart();
+        //this.requestStart();
        /* if(this.ridx === null) {
 
         }*/
--- src/pages/home/my-car/MyCar.js
+++ src/pages/home/my-car/MyCar.js
@@ -127,11 +127,11 @@ export default class MyCar extends Component {
                     }}
                     fetchData={(pageNumber, pageSize) => {
                         return queryRealTimeCarList(pageNumber, pageSize, this.state.key);
-                        /*
-                        * return queryRealTimeCarList(pageNumber, pageSize, this.state.key).then((data) => {
+
+                        {/*return queryRealTimeCarList(pageNumber, pageSize, this.state.key).then((data) => {
                          data.list.length = 1;
                          return data;
-                         });*/
+                         });*/}
                     }}
                     noMore="已经没有更多车辆了"
                 />
