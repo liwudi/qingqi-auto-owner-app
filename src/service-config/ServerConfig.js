ede0cbf3021aa86209e97cabf65b733a9bc09003
ligj <ligj@mapbar.com>
2016-12-18 10:51:05 +0800

    修改域名

--- src/service-config/ServerConfig.js
+++ src/service-config/ServerConfig.js
@@ -6,22 +6,27 @@ if(!DEBUG) {
     console.info = console.log = () => {}
 }
 const ServerConfig = {
-    QINGQI: 'http://61.161.238.158:8950/qingqi/',
-    // QINGQI: 'http://10.30.50.152:8950/qingqi/',
+    // QINGQI: 'http://61.161.238.158:8950/qingqi/',
+    QINGQI: 'http://jfx.mapbar.com/api/qingqi/',
+    // QINGQI: 'http://10.30.50.151:8950/qingqi/',
 
     //WD_SERVICE: 'https://wdservice.mapbar.com/ssoapi/',
     //WEB_PAGE: 'http://119.255.37.167:2220/',
-	WEB_PAGE: 'http://192.168.85.33:2020/',
-    // WD_SERVICE: 'http://192.168.85.49/',
-    WD_SERVICE: 'http://119.255.37.167:8808/',
 
-    BBS_PAGE: 'http://61.161.238.158:8071/mapbar/yqlt.php',
+    // WD_SERVICE: 'http://192.168.85.49/',
+    // WD_SERVICE: 'http://119.255.37.167:8808/',
+    WD_SERVICE: 'http://jfx.mapbar.com/usercenter/',
+
+    BBS_PAGE: 'http://jfx.mapbar.com/forum/yqlt.php',
 
     GOODS_PAGE: 'https://www.lujing56.com/activities/goodsource/view/find_goods.html',
 
     NEWS_SERVICE: 'http://219.146.249.190:10106/',
 
-    IMG_SERVICE: 'https://wdservice.mapbar.com/ssoapi/user/queryPicById',//用于头像相关
+    IMG_SERVICE: 'http://jfx.mapbar.com/usercenter/user/queryPicById',//用于头像相关
+
+    UPLOAD_SERVICE: 'http://jfx.mapbar.com/fsm/',
+
     defaultPage : {
         page_number: 1,
         page_size: 20,
