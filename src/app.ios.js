6b267cf7e163cf0a71c335fc15f7c1bacc22586b
liufang <liufang@mapbar.com>
2016-12-20 14:08:09 +0800

    ios

--- src/app.ios.js
+++ src/app.ios.js
@@ -5,22 +5,13 @@ import { Provider } from 'react-redux'
 
 import configureStore from './configure-store';
 
-import Main from './pages/Main.android';
+import Main from './pages/Main';
 import Env from './utils/Env';
 
 const estyle = Env.style;
 import ServerConfig from './service-config/ServerConfig';
 
-//获取设备id
-var pushModule = NativeModules.MarbarPushModule;
 
-pushModule.getDeviceId().then((r) => {
-	global.deviceId = r.deviceId;
-	ServerConfig.DEVICE_ID = r.deviceId;
-	console.log('设备id:', r.deviceId)
-}, (e) => {
-	global.deviceId = null;
-});
 
 AppState.addEventListener('change', (currentAppState) => {
     global.appIsActive = (currentAppState == 'active');
